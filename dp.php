93a8d44e239b5aeae8c0fd6fd8b4888c918c0617
DEEMA ALQUWAEI <445200256@student.ksu.edu.sa>
2026-06-12 16:45:14 +0300

    Remove database password from dp.php
    Remove database password for security reasons.

--- dp.php
+++ dp.php
@@ -2,7 +2,7 @@
 $host = 'sql303.infinityfree.com';
 $db   = 'if0_41939924_sufrah';
 $user = 'if0_41939924';
-$pass = 'XADHs6Pes4tAiv';
+$pass = '';
 
 $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
